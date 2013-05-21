6ef8f6854b27b5f6e96ebfb813497188c50f202d
Mikhail Golubev <qsolo825@gmail.com>
2013-05-21 11:34:19 +0400

    Scripts with build dependencies for python2.7/3.3

---
+++ python-dependencies-2.7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get install \
+    zlib1g-dev \
+    libbz2-dev \
+    libncurses5-dev \
+    libreadline6-dev \
+    tk8.5-dev \
+    tcl8.5-dev \
+    libsqlite3-dev \
+    libgdbm-dev \
+    libdb-dev\
+    libssl-dev 
