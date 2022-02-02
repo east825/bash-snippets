af082dfe92f6eeab34a681f8de49b7d9120b77a2
Mikhail Golubev <qsolo825@gmail.com>
2022-02-02 17:29:10 +0200

    Add an initial list of packages I normally use on Linux

---
+++ bootstrap-debian.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# set -x
+
+# Command-line tools
+sudo apt-get install \
+  git \
+  net-tools \
+  zsh \
+  mc \
+  autojump \
+  htop \
+  ncdu \
+  tree \
+  httpie \
+  apt-file
+
+# Graphical tools
+sudo apt-get install \
+  redshift-gtk \
+  flameshot \
+  diodon
